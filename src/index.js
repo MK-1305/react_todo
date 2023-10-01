afeed353ef6fec6aa6cea5f5f4b85fa46e5c60d2
MK-1305 <66237794+MK-1305@users.noreply.github.com>
2023-10-01 18:57:17 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { App } from "./App";
+
+// index.htmlのrootに対してレンダリングさせる記述
+ReactDOM.render(<App />, document.getElementById("root"));
